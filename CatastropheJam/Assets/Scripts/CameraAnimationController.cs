9ecde2859c26e85983d2603841a89ca4622c20b0
Rob Butler <robbutler902@gmail.com>
2014-12-12 19:04:41 -0400

    It movesgit addallgit addall

---
+++ CatastropheJam/Assets/Scripts/CameraAnimationController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAnimationController : MonoBehaviour {
+
+	protected Quaternion originalRotation;
+	protected Vector3 originalPosition;
+
+	[SerializeField] protected Animator animator;
+	[SerializeField] protected Transform playerTransform;
+
+	protected bool animating;
+
+	void Awake(){
+		originalPosition = transform.localPosition;
+		originalRotation = transform.localRotation;
+		animating = true;
+	}
+
+	public IEnumerator ToggleAnimation(bool toggle){
+		animating = toggle;
+
+		// lerp to original stuff and enable animation
+		if (toggle){
+			yield return StartCoroutine(ReturnToOriginalPosition());
+		}
+		// cancel animation
+		else{
+			animating = false;
+			animator.enabled = false;
+		}
+
+		yield break;
+	}
+
+	void Update(){
+		if (animating){
+			transform.LookAt(playerTransform, Vector3.up);
+		}
+	}
+
+	IEnumerator ReturnToOriginalPosition(){
+		float distanceThreshold = .1f;
+		float angleThreshold = 5;
+		float angleStep = 2;
+		float translationStep = .1f;
+
+		Vector3 basePos = transform.localPosition;
+		float progress = .01f;
+		while (Vector3.Distance(originalPosition, transform.localPosition) > distanceThreshold ||
+			   Quaternion.Angle(originalRotation, transform.localRotation) > angleThreshold){
+
+				transform.localRotation = Quaternion.RotateTowards(transform.localRotation, originalRotation, angleStep);
+				transform.localPosition = Vector3.Lerp(basePos, originalPosition, progress);
+
+				progress += translationStep;
+				yield return null;
+		}
+
+
+	}
+}
---
+++ CatastropheJam/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	float rotationSpeed = .5f,
+		  forwardSpeed = .1f,
+		  sideSpeed = .1f;
+
+	bool movingForward = false,
+		 movingBackward = false,
+		 movingRight = false,
+		 movingLeft = false;
+
+	bool canMove = true;
+
+	[SerializeField] protected Animator babyAnimator;
+
+	public bool CanMove{
+		get { return canMove; }
+		set { canMove = value;}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (canMove){
+			Vector3 position = transform.localPosition;
+
+			// Left
+			if (Input.GetKeyDown(KeyCode.A)){
+				movingLeft = true;
+				movingRight = false;
+				babyAnimator.SetBool("walking", true);
+			}
+			// Up
+			else if (Input.GetKeyDown(KeyCode.W)){
+				movingBackward = false;
+				movingForward = true;
+				babyAnimator.SetBool("walking", true);
+			}
+			// Down
+			else if (Input.GetKeyDown(KeyCode.S)){
+				movingBackward = true;
+				movingForward = false;
+				babyAnimator.SetBool("walking", true);
+			}
+			//Right
+			else if (Input.GetKeyDown(KeyCode.D)){
+				movingLeft = false;
+				movingRight = true;
+				babyAnimator.SetBool("walking", true);
+			}
+
+			// Left
+			if (Input.GetKeyUp(KeyCode.A)){
+				movingLeft = false;
+				babyAnimator.SetBool("walking", false);
+			}
+			// Up
+			else if (Input.GetKeyUp(KeyCode.W)){
+				movingForward = false;
+				babyAnimator.SetBool("walking", false);
+			}
+			// Down
+			else if (Input.GetKeyUp(KeyCode.S)){
+				movingBackward = false;
+				babyAnimator.SetBool("walking", false);
+			}
+			//Right
+			else if (Input.GetKeyUp(KeyCode.D)){
+				movingRight = false;
+				babyAnimator.SetBool("walking", false);
+			}
+
+			if (movingForward){
+				position.z += sideSpeed;
+			}
+			else if (movingBackward){
+				position.z -= sideSpeed;
+			}
+			else if (movingRight){
+				position.x += forwardSpeed;
+			}
+			else if (movingLeft){
+				position.x -= forwardSpeed;
+			}
+
+			float rotation = Input.GetAxis("Mouse X") * rotationSpeed;
+			transform.Rotate(0, rotation, 0);
+
+			transform.localPosition = position;
+		}
+	}
+}
